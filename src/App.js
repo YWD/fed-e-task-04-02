2241f53a027b17bbef648fc977dd7798d63fb16e
杨文冬 <1347011912@qq.com>
2022-06-02 10:44:17 +0800

    feat: ui修改

--- src/App.js
+++ src/App.js
@@ -17,18 +17,19 @@ function App() {
         </p>
       </header>
       <div className="container">
-        <div className="link">
-          <Link className="link-href" to="/">Index Page</Link>
-          <Link className="link-href" to="/mobx">Mobx State Page</Link>
-          <Link className="link-href" to="/hooks">Hooks State Page</Link>
-          <Link className="link-href" to="/cart">CartStore Page</Link>
-        </div>
-        <div className="route-container">
-          <Route path="/" component={IndexPage} exact={true}></Route>
-          <Route path="/mobx" component={AppleComponent}></Route>
-          <Route path="/hooks" component={HooksWrapper}></Route>
-          <Route path="/cart" component={CartPage}></Route>
-        </div>
+        <AppleComponent />
+        {/*<div className="link">*/}
+        {/*  /!*<Link className="link-href" to="/">Index Page</Link>*!/*/}
+        {/*  <Link className="link-href" to="/mobx">Mobx State Page</Link>*/}
+        {/*  /!*<Link className="link-href" to="/hooks">Hooks State Page</Link>*!/*/}
+        {/*  /!*<Link className="link-href" to="/cart">CartStore Page</Link>*!/*/}
+        {/*</div>*/}
+        {/*<div className="route-container">*/}
+        {/*  /!*<Route path="/" component={IndexPage} exact={true}></Route>*!/*/}
+        {/*  <Route path="/mobx" component={AppleComponent}></Route>*/}
+        {/*  /!*<Route path="/hooks" component={HooksWrapper}></Route>*!/*/}
+        {/*  /!*<Route path="/cart" component={CartPage}></Route>*!/*/}
+        {/*</div>*/}
       </div>
     </div>
   );
